6946868d7f73d44c7ac5db9ffdbf9c7447b434b2
Bellonor <Bellonor@126.com>
2014-05-20 00:44:43 +0800

    函数还没写好，明天继续

--- Apriori/apriori_0.r
+++ Apriori/apriori_0.r
@@ -1,6 +1,23 @@
 #1. 在算法的第一次迭代，每个项都是候选1-项集的集合C1 的成员。算法简单地扫描所有的事务，
 #   对每个项的出现次数计数。
-data<-read.csv("Apriori/data.csv",sep = ",", fill = TRUE,header=FALSE);
-data<-read.table("Apriori/data.csv",header=FALSE);
-mode(data)
-data
+support<-2;#支持度设为2
+data<-read.csv("Apriori/data.csv",sep = ",",row.names=1, fill = TRUE,header=FALSE,col.names = paste0("V", 1:5));
+rows=length(data[,1]);
+cols=length(data[1,]);
+c1<-matrix(0,nrow=0,ncol=2);
+
+for(i in 1:rows){
+  for(j in 1:cols){
+    browser() 
+    tmp<-data[i,j];
+     index<-which(c1[,1]==tmp[[1]]);
+     if(length(index)==0){
+       t1<-cbind(as.character(tmp[[1]]),'1');
+       c1<-rbind(c1,t1);
+     }else{
+       tmp2<-as.numeric(c1[index,2])+1;
+       c1[index,2]<-tmp2;
+     }
+  }
+}
+#122
