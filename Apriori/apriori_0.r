99083fe326a0ab504ec17b10b16ed2eb74d3da20
Bellonor <bellonor@126.com>
2014-05-20 15:59:15 +0800

    假定最小事务支持计数为2（即，min_sup = 2/9 = 22%）。可以确定频繁1-项集的集合L1。它由 具有最小支持度的候选1-项集组成。

--- Apriori/apriori_0.r
+++ Apriori/apriori_0.r
@@ -1,18 +1,37 @@
+#我的函数,字符串为否为空
+library(stringr)
+isEmptyStr<-function(str){
+  str<-str_trim(str)
+  if(str==""){
+    return (TRUE);
+  }
+  if(length(str)==0){
+    return (TRUE);
+  }
+  if(is.na(str)){
+    return (TRUE);
+  }else{
+    return (FALSE);
+  }
+}
+a<-isEmptyStr("")
 #1. 在算法的第一次迭代，每个项都是候选1-项集的集合C1 的成员。算法简单地扫描所有的事务，
 #   对每个项的出现次数计数。
 support<-2;#支持度设为2
 data<-read.csv("Apriori/data.csv",sep = ",",row.names=1, fill = TRUE,header=FALSE,col.names = paste0("V", 1:5));
-rows=length(data[,1]);
-cols=length(data[1,]);
+rows<-length(data[,1]);
+cols<-length(data[1,]);
 c1<-matrix(0,nrow=0,ncol=2);
-
 for(i in 1:rows){
   for(j in 1:cols){
-    browser() 
+    
     tmp<-data[i,j];
-     index<-which(c1[,1]==tmp[[1]]);
+    tmpstr<-as.character(tmp[[1]]);
+    isemp<-isEmptyStr(tmpstr);
+    if(isemp)next
+    index<-which(c1[,1]==tmpstr);
      if(length(index)==0){
-       t1<-cbind(as.character(tmp[[1]]),'1');
+       t1<-cbind(tmpstr,'1');
        c1<-rbind(c1,t1);
      }else{
        tmp2<-as.numeric(c1[index,2])+1;
@@ -20,4 +39,14 @@ for(i in 1:rows){
      }
   }
 }
-#122
+l1<-c1[order(c1[,1]),]
+c1rows<-length(c1[,1]);
+for(i in 1:c1rows){
+ 
+  tmp3<-as.numeric(c1[i,2]);
+  if(tmp3<support){
+    index<-which(l1[,1]==c1[i,1])
+    l1<-l1[-index,];
+  }
+}
+  
